2551b3744de1aa098d5cf12c647187ebd6d2d2a4
WebDevKraken <webdevepak@gmail.com>
2024-05-16 19:17:12 +0530

    1st commit

---
+++ signin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/signin.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Sign in</title>
+</head>
+<body>
+    <div class="container">
+            <div class="login">
+                <i class="bi bi-person"></i>
+                <h3 class="head">Login</h3>
+                <p class="para">You will be directed to the homepage</p>
+                <form action="">
+                    <div class="users">
+                        <input type="email" placeholder="Email">
+                        <input type="password" placeholder="Password">
+                    </div>
+                    <input type="submit" value="submit" class="btn">
+                </form>
+                <a href="">Forgot Password?</a>
+            </div>
+    </div>
+</body>
+</body>
+</html>
